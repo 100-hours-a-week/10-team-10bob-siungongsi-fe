cf540ad1fc23c8a379609d245b4716e8680ae706
민항섭 <minhangseob@MacBook-Air.local>
2025-03-12 19:58:03 +0900

    feat: 로그인슬라이더 구현

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import { LoginSlider } from "./LoginSlider";
 
 export const Header = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
   return (
     <div className="flex justify-between items-center p-4 bg-primary text-white font-bold max-h-[55px]">
       <h1 className="text-2xl">Siun</h1>
-      <button className="bg-white p-2 rounded-lg text-primary">로그인</button>
+      <button
+        className="bg-white p-2 rounded-lg text-primary"
+        onClick={() => setIsOpen(true)}
+      >
+        로그인
+      </button>
+      {isOpen && (
+        <LoginSlider isOpen={isOpen} onClose={() => setIsOpen(false)} />
+      )}
     </div>
   );
 };
--- src/components/LoginSlider.tsx
+++ src/components/LoginSlider.tsx
@@ -1,14 +1,39 @@
 import React from "react";
+import { motion } from "framer-motion";
 
-export const LoginSlider = () => {
+interface Props {
+  isOpen: boolean;
+  onClose: any;
+}
+
+export const LoginSlider = ({ isOpen, onClose }: Props) => {
+  if (!isOpen) return null;
   return (
-    <div className="fixed w-[392px] left-30 z-50 p-4 border bg-white">
-      <header className="flex justify-between m-4">
-        <div>로그인</div>
-        <div>X</div>
-      </header>
-      <div className="flex justify-center">카카오 로그인</div>
-      <div className="flex justify-center">회원가입 하시겠습니까?</div>
+    <div
+      className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-[1000]"
+      onClick={onClose}
+    >
+      <motion.div
+        className="fixed bottom-0 bg-white p-6 rounded-tl-xl rounded-tr-xl shadow-xl w-96 max-h-[290px]"
+        initial={{ y: "100vh", opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }} //이거 만약 모바일로 옮기면 수치 수정해야겠음
+        exit={{ y: "100vh", opacity: 0 }}
+        transition={{ duration: 0.6, ease: "easeOut" }}
+        onClick={(e) => e.stopPropagation()} // 모달 내부 클릭시 닫히지 않도록
+      >
+        <div className="flex justify-between mb-4">
+          <h2 className="text-xl font-bold text-black">로그인</h2>
+          <button className="text-black rounded-md" onClick={onClose}>
+            X
+          </button>
+        </div>
+        <div className="flex flex-col p-8 items-center gap-8">
+          <button className="bg-yellow-400">카카오로 로그인</button>
+          <button className="text-gray-300 font-normal">
+            회원가입 하시겠습니까?
+          </button>
+        </div>
+      </motion.div>
     </div>
   );
 };
