c85509e31258bb856d7373ec7892d6d05291e1b4
민항섭 <minhangseob@MacBook-Air.local>
2025-03-12 20:02:21 +0900

    fix: 버그 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import "./App.css";
 import { Main } from "./pages/Main/Main";
-import { GongsiSearch } from "./pages/GongsiSearch/GongsiSearch";
 
 function App() {
   return (
     <div>
       <Main></Main>
-      {/* <GongsiSearch /> */}
     </div>
   );
 }
