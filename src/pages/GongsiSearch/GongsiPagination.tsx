31af6ca8e128b1c2b3284db95424e065bd172e0f
민항섭 <minhangseob@MacBook-Air.local>
2025-03-20 13:48:24 +0900

    feat: 공시목록 불러오기

--- src/pages/GongsiSearch/GongsiPagination.tsx
+++ src/pages/GongsiSearch/GongsiPagination.tsx
@@ -1,42 +1,76 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { gongsiTitleList } from "../Main/dummyTitle";
 import { GongsiList } from "../../components/GongsiList";
 import { useNavigate } from "react-router-dom";
+import { GongsiData, fetchGongsiList } from "../../services/gongsiService";
+import { LoadingSpinner } from "../../components/LoadingSpinner";
 
 export const GongsiPagination = () => {
-  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState<boolean>();
   const [currentPage, setCurrentPage] = useState<number>(1);
-  const postsPerPage = 9;
+  useEffect(() => {
+    const getGongsiList = async () => {
+      try {
+        setIsLoading(true);
+        const response = await fetchGongsiList(
+          null,
+          "latest",
+          false,
+          currentPage,
+          8,
+          null,
+          null,
+        );
 
-  const startIndex = (currentPage - 1) * postsPerPage;
-  const endIndex = startIndex + postsPerPage;
-  const currentPosts = gongsiTitleList.slice(startIndex, endIndex);
+        setGongsiData(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error("공시목록 조회 에러 : ", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    getGongsiList();
+  }, [currentPage]);
+  const [gongsiData, setGongsiData] = useState<GongsiData>();
+  const navigate = useNavigate();
 
-  const totalPages = Math.ceil(gongsiTitleList.length / postsPerPage);
+  // const postsPerPage = 9;
+
+  // const startIndex = (currentPage - 1) * postsPerPage;
+  // const endIndex = startIndex + postsPerPage;
+  // const currentPosts = gongsiTitleList.slice(startIndex, endIndex);
+
+  const totalPages = gongsiData?.pagination.totalPages;
   return (
     <div className="w-full max-w-xl mx-auto">
       {/* 게시글 목록 */}
-      <ul className="border-b border-gray-300 divide-y">
-        {currentPosts ? (
-          currentPosts.map((post) => (
-            <li
-              onClick={() => navigate("/detail")}
-              key={post.id}
-              className="p-3"
-            >
-              <div className="font-bold truncate">{post.title}</div>
-              <div className="text-gray-500 text-sm">{post.company}</div>
+      <ul className="flex flex-col max-h-content">
+        {!isLoading ? (
+          gongsiData?.gongsiList.map((gongsi) => (
+            <li key={gongsi.gongsiId} className="border-b border-gray-300 p-3">
+              <div className="font-bold truncate">{gongsi.gongsiTitle}</div>
+              <div className="flex justify-between">
+                <div className="text-gray-500 text-sm">
+                  {gongsi.companyName}
+                </div>
+                <div className="text-gray-500 text-sm">
+                  {gongsi.publishedDatetime.toString()}
+                </div>
+              </div>
             </li>
           ))
         ) : (
-          <div>해당하는 공시가 없습니다</div>
+          <div className="flex h-[545px] items-center justify-center">
+            <LoadingSpinner />
+          </div>
         )}
       </ul>
       <div className="flex justify-center mt-4 space-x-2">
         <button
           onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
-          disabled={currentPage === 1}
-          className={`px-3 py-1 border rounded-md ${currentPage === 1 ? "text-gray-400 border-gray-300" : "hover:bg-gray-100"}`}
+          disabled={currentPage === 1 || isLoading}
+          className={`px-3 py-1 border rounded-md ${currentPage === 1 || isLoading ? "text-gray-400 border-gray-300" : "hover:bg-gray-100"}`}
         >
           이전
         </button>
@@ -44,19 +78,18 @@ export const GongsiPagination = () => {
         {[...Array(totalPages)].map((_, index) => (
           <button
             key={index}
+            disabled={isLoading}
             onClick={() => setCurrentPage(index + 1)}
-            className={`px-3 py-1 border rounded-md ${currentPage === index + 1 ? "bg-blue-500 text-white" : "hover:bg-gray-100"}`}
+            className={`px-3 py-1 border rounded-md ${currentPage === index + 1 ? "bg-blue-500 text-white" : "hover:bg-gray-100"} ${isLoading ? "text-gray-400 border-gray-300" : ""}`}
           >
             {index + 1}
           </button>
         ))}
 
         <button
-          onClick={() =>
-            setCurrentPage((prev) => Math.min(prev + 1, totalPages))
-          }
-          disabled={currentPage === totalPages}
-          className={`px-3 py-1 border rounded-md ${currentPage === totalPages ? "text-gray-400 border-gray-300" : "hover:bg-gray-100"}`}
+          onClick={() => setCurrentPage((prev) => prev + 1)}
+          disabled={currentPage === totalPages || isLoading}
+          className={`px-3 py-1 border rounded-md ${currentPage === totalPages || isLoading ? "text-gray-400 border-gray-300" : "hover:bg-gray-100"}`}
         >
           다음
         </button>
--- src/services/gongsiService.ts
+++ src/services/gongsiService.ts
@@ -1,20 +1,45 @@
 import api from "../api/api";
-
-interface termsOfUse {
-  id: number;
-  title: string;
-  content: string;
+interface APIData {
+  code: number;
+  message: string;
+  data: GongsiData;
 }
-
+export interface GongsiData {
+  gongsiList: Gongsi[];
+  gongsiListSize: number;
+  pagination: pagination;
+}
+interface Gongsi {
+  gongsiId: number;
+  gongsiTitle: string;
+  companyName: string;
+  publishedDatetime: Date;
+  content: null;
+}
+interface pagination {
+  currentPage: number;
+  totalPages: number;
+  totalResults: number;
+}
 const apiKey = process.env.REACT_APP_API_URL;
 
-// 약관 불러오기
-export const fetchTermsOfUse = async (): Promise<termsOfUse[]> => {
+// 공시목록 불러오기
+export const fetchGongsiList = async (
+  companyId: number | null,
+  sortCondition: string | null,
+  contentVisible: boolean | null,
+  page: number | null,
+  size: number | null,
+  startDate: Date | null,
+  endDate: Date | null,
+): Promise<APIData> => {
   try {
-    const response = await api.get<termsOfUse[]>(`${apiKey}auth/terms`);
+    const response = await api.get<APIData>(
+      `${apiKey}gongsi?companyId=${companyId ? companyId : ""}&sort=${sortCondition ? sortCondition : ""}&content=${contentVisible ? contentVisible : ""}&page=${page ? page : ""}&size=${size ? size : ""}&startDate=${startDate ? startDate : ""}&endDate=${endDate ? endDate : ""}`,
+    );
     return response.data;
   } catch (error) {
-    console.error("이용 약관 불러오기 에러: ", error);
+    console.error("공시목록 불러오기 에러: ", error);
     throw error;
   }
 };
